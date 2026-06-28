13f05947a1083edbfa93c3b38ba892ab64bf3992
Vitaliy Kukharik <vitabaks@gmail.com>
2026-06-28 23:01:46 +0200

    Remove ClickEffect from Root
    Remove the ClickEffect import and its BrowserOnly mounting from src/theme/Root.js. This stops the ClickEffect component from being rendered alongside children and SideNavbar, leaving other behavior unchanged.

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 import { useLocation } from '@docusaurus/router';
-import ClickEffect from '@site/src/components/ClickEffect';
 import SideNavbar from '@site/src/components/SideNavbar';
 
 function SideNavbarGate() {
@@ -14,7 +13,6 @@ export default function Root({ children }) {
   return (
     <>
       {children}
-      <BrowserOnly>{() => <ClickEffect />}</BrowserOnly>
       <BrowserOnly>{() => <SideNavbarGate />}</BrowserOnly>
     </>
   );
